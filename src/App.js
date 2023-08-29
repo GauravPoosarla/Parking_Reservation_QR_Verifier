16e0c202a1fa694e7f43431c9115f4ff6d2e9e44
Gaurav Poosarla <poosarlagaurav@gmail.com>
2023-08-29 21:05:57 +0530

    feat: update styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [scannedData, setScannedData] = useState("");
   const [apiResponse, setApiResponse] = useState(null);
   const [reservationValid, setReservationValid] = useState(null);
+  const [showQrReader, setShowQrReader] = useState(false);
 
   const handleScan = async (data) => {
     setScannedData(data.text);
@@ -21,7 +22,7 @@ function App() {
       };
 
       const response = await axios.post(
-        "https://deorsum.serveo.net/verify-qr", //change this server url everything you run from serveo
+        "https://dudum.serveo.net/verify-qr", //change this server url everything you run from serveo and publish again
         requestData
       );
 
@@ -52,31 +53,44 @@ function App() {
       <h1 className="text-3xl font-semibold mb-4 text-purple-700">
         QR Code Validator
       </h1>
-      <QrReader
-        onError={handleError}
-        onScan={handleScan}
-        style={{ width: "80%", maxWidth: "300px" }}
-        facingMode="environment"
-      />
-      <div className="mt-4">
-        <h2 className="text-xl font-semibold mb-2 text-purple-700">
-          Scanned Data:
-        </h2>
-        <div>
-          <pre className="mt-2 text-gray-700 whitespace-pre-wrap">
-            {JSON.stringify(apiResponse, null, 2)}
-          </pre>
-        </div>
+      <button className="bg-purple-700 text-white px-4 py-2 rounded-md mb-4" onClick={() => setShowQrReader(!showQrReader)}>
+        {showQrReader ? "Hide QR Scanner" : "Show QR Scanner"}
+      </button>
+      <div className="mb-4">
+        {showQrReader && (
+          <QrReader
+          onError={handleError}
+          onScan={handleScan}
+          style={{ width: "80%", maxWidth: "300px"}}
+          facingMode="environment"
+        />)}
       </div>
       <div className="mt-4">
-        <h2 className="text-xl font-semibold mb-2 text-purple-700">
-          Response from server:
+        <h2 className="text-xl font-semibold mb-2">
+          Reservation:
         </h2>
         {apiResponse ? (
           <div>
-            <pre className="mt-2 text-gray-700 whitespace-pre-wrap">
-              {JSON.stringify(apiResponse, null, 2)}
-            </pre>
+            <table className="table-auto">
+              <thead className="bg-purple-700 text-white"> 
+                <tr>
+                  <th className="px-4 py-2">Slot</th>
+                  <th className="px-4 py-2">Start Time</th>
+                  <th className="px-4 py-2">End Time</th>
+                  <th className="px-4 py-2">Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border px-4 py-2">{apiResponse.slot}</td>
+                  <td className="border px-4 py-2">
+                    {apiResponse.startTime}
+                  </td>
+                  <td className="border px-4 py-2">{apiResponse.endTime}</td>
+                  <td className="border px-4 py-2">{apiResponse.date}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         ) : (
           <p className="text-gray-700">No response yet</p>
