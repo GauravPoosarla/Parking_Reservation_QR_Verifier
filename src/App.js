8408092a49fba30bf04af13f5cd5fbc07f3cfc21
Gaurav Poosarla <poosarlagaurav@gmail.com>
2023-08-29 16:01:46 +0530

    feat: add entire working application for qr verify

--- src/App.js
+++ src/App.js
@@ -1,56 +1,97 @@
-import React, { useState } from 'react';
-import QrScanner from 'react-qr-scanner';
+import React, { useState } from "react";
+import QrReader from "react-qr-scanner";
+import axios from "axios";
 
 function App() {
-  const [scannedData, setScannedData] = useState('');
+  const [scannedData, setScannedData] = useState("");
   const [apiResponse, setApiResponse] = useState(null);
+  const [reservationValid, setReservationValid] = useState(null);
 
-  const handleScan = async data => {
-    if (data) {
-      setScannedData(data);
+  const handleScan = async (data) => {
+    setScannedData(data.text);
 
-      try {
-        const response = await fetch('http://localhost:8000/verify-qr', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ qrCodeData: data }),
-        });
+    try {
+      const mainObject = JSON.parse(data.text);
+      const { slot, startTime, endTime, date } = mainObject;
+      const requestData = {
+        slot,
+        startTime,
+        endTime,
+        date,
+      };
 
-        if (response.ok) {
-          const responseData = await response.json();
-          setApiResponse(responseData);
+      const response = await axios.post(
+        "https://deorsum.serveo.net/verify-qr", //change this server url everything you run from serveo
+        requestData
+      );
+
+      if (response) {
+        setApiResponse(response.data);
+        if (response.data === "Reservation not found") {
+          setReservationValid(false);
         } else {
-          console.error('API call failed with status:', response.status);
-          setApiResponse(null);
+          setReservationValid(true);
         }
-      } catch (error) {
-        console.error('API call error:', error);
-        setApiResponse(null);
+        alert(
+          "API call successful. Response: " + JSON.stringify(response.data)
+        );
       }
+    } catch (error) {
+      alert("Error processing QR code data: " + error);
+      setApiResponse(null);
+      setReservationValid(false);
     }
   };
 
-  const handleError = err => {
-    console.error(err);
+  const handleError = (err) => {
+    alert(err);
   };
 
   return (
-    <div className="App">
-      <h1>QR Code Verifier</h1>
-      <QrScanner
-        onScan={handleScan}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-purple-100">
+      <h1 className="text-3xl font-semibold mb-4 text-purple-700">
+        QR Code Validator
+      </h1>
+      <QrReader
         onError={handleError}
-        style={{ width: '100%' }}
+        onScan={handleScan}
+        style={{ width: "80%", maxWidth: "300px" }}
+        facingMode="environment"
       />
-      <div>
-        <h2>Scanned Data:</h2>
-        <p>{scannedData}</p>
+      <div className="mt-4">
+        <h2 className="text-xl font-semibold mb-2 text-purple-700">
+          Scanned Data:
+        </h2>
+        <div>
+          <pre className="mt-2 text-gray-700 whitespace-pre-wrap">
+            {JSON.stringify(apiResponse, null, 2)}
+          </pre>
+        </div>
+      </div>
+      <div className="mt-4">
+        <h2 className="text-xl font-semibold mb-2 text-purple-700">
+          Response from server:
+        </h2>
+        {apiResponse ? (
+          <div>
+            <pre className="mt-2 text-gray-700 whitespace-pre-wrap">
+              {JSON.stringify(apiResponse, null, 2)}
+            </pre>
+          </div>
+        ) : (
+          <p className="text-gray-700">No response yet</p>
+        )}
       </div>
       <div>
-        <h2>API Response:</h2>
-        <p>{apiResponse ? JSON.stringify(apiResponse) : 'No response yet'}</p>
+        <p
+          className={`${
+            reservationValid ? "text-green-600" : "text-red-600"
+          } font-bold`}
+        >
+          {reservationValid
+            ? "Reservation is valid"
+            : "Reservation is not valid"}
+        </p>
       </div>
     </div>
   );
